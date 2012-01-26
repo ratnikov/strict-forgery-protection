a6a915ec6ec3917bee69ea98586e53ce83db3c29
Dmitry Ratnikov <ratnikov@google.com>
2012-01-26 17:10:23 -0500

    Added some extra things to test in the test.

--- test/db_events_test.rb
+++ test/db_events_test.rb
@@ -1,25 +1,46 @@
 require 'test_helper'
 
 class DbEventsTest < ActiveSupport::TestCase
-  def test_reads
+  def test_read_queries
     assert_reads do
-      execute "SELECT * from posts; DELETE from posts"
+      Post.all
+      Post.count
+
+      execute "SELECT * from posts"
+      execute "SELECT total_changes()"
+
+      Post.find_by_id 42
     end
   end
 
-  def test_writes
+  def test_record_modification
     assert_writes do
-      execute "INSERT INTO posts ('message') VALUES ('hello world')"
-      execute "DELETE from posts"
+      post = Post.create! :message => 'hello world'
+      Post.delete_all
+      post.update_attributes :message => 'goodbye'
+      post.destroy
+    end
+  end
+
+  def test_raw_writes
+    assert_writes do
+      execute "CREATE TABLE foos (foo INT)"
+      execute "INSERT INTO foos (foo) VALUES (5)"
+      execute "DELETE FROM foos"
+      execute "DROP TABLE foos"
     end
   end
 
   private
 
   def assert_reads
+    previous_changes = total_changes
+
     events = sql_events { yield }
 
     assert events.all?(&:read?), "Expected all events to be read, but got: #{events.map { |e| [e, e.read? ] }.inspect}"
+
+    assert_equal 0, total_changes - previous_changes, "Expected no new changes"
   end
 
   def assert_writes
@@ -34,6 +55,10 @@ class DbEventsTest < ActiveSupport::TestCase
     yield
 
     ActiveRecord::QueryTracker.sql_events
+  end
+
+  def total_changes
+    execute("SELECT total_changes()").first['total_changes()']
   end
 
   def execute(sql)
