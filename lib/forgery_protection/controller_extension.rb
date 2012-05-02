752d447b16b798e999892db129ad3e2418fc802d
Dmitry Ratnikov <ratnikov@google.com>
2012-05-02 16:44:27 -0400

    Made the logic muuuuch simpler by copying the logic of checking forgery protection token from rails

--- lib/forgery_protection/controller_extension.rb
+++ lib/forgery_protection/controller_extension.rb
@@ -12,32 +12,19 @@ module ForgeryProtection
 
     protected
 
-    def verify_authenticity_token
-      verify_request! if protect_against_forgery?
-    end
-
-    def verify_request!
-      if form_authenticity_token == params[request_forgery_protection_token] ||
-        form_authenticity_token == request.headers['X-CSRF-Token']
-
-        @request_verified = true
-      else
-        handle_unverified_request
-      end
-    end
-
-    def verified_request?
-      !!@request_verified
-    end
-
     def detect_unverified_db_update
       ForgeryProtection::QueryTracker.reset_sql_events
 
       yield.tap do
-	if ForgeryProtection::QueryTracker.sql_events.any? { |e| e.write? } && !verified_request?
+	unless valid_forgery_protection_token? || ForgeryProtection::QueryTracker.sql_events.none? { |e| e.write? }
           raise AttemptError, "A database update occurred for an unverified request"
 	end
       end
+    end
+
+    def valid_forgery_protection_token?
+      form_authenticity_token == params[request_forgery_protection_token] ||
+      form_authenticity_token == request.headers['X-CSRF-Token']
     end
 
     def handle_unverified_request
