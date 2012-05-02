407435ca0d15ca4b0c08fa1fc0d59d308826682c
Dmitry Ratnikov <ratnikov@google.com>
2012-05-02 13:13:15 -0400

    Changed the behavior of the gem to require request verification having occured of DB changes are detected.
    This will now trip up an application if a POST request skips
    verification but introduces DB changes.
    
    Bumped version, since this is a big change.

--- lib/forgery_protection/controller_extension.rb
+++ lib/forgery_protection/controller_extension.rb
@@ -3,29 +3,47 @@ module ForgeryProtection
 
   module ControllerExtension
     def self.included(controller)
-      controller.around_filter :verify_strict_authenticity
+      controller.around_filter :detect_unverified_db_update
 
-      def controller.skip_forgery_protection(*args)
-        skip_filter :verify_authenticity_token, *args
-        skip_filter :verify_strict_authenticity, *args
+      def controller.permit_unverified_state_changes(*args)
+        skip_filter :detect_unverified_db_update, *args
       end
     end
 
-    private
+    protected
 
-    def verify_strict_authenticity
+    def verify_authenticity_token
+      verify_request! if protect_against_forgery?
+    end
+
+    def verify_request!
+      if form_authenticity_token == params[request_forgery_protection_token] ||
+        form_authenticity_token == request.headers['X-CSRF-Token']
+
+        @request_verified = true
+      else
+        handle_unverified_request
+      end
+    end
+
+    def verified_request?
+      !!@request_verified
+    end
+
+    def detect_unverified_db_update
       ForgeryProtection::QueryTracker.reset_sql_events
 
       yield.tap do
-	provided_tokens = [ request.headers['X-CSRF-Token'], params[request_forgery_protection_token] ].compact
-	if ForgeryProtection::QueryTracker.sql_events.any? { |e| e.write? } && !provided_tokens.include?(form_authenticity_token) 
-	  handle_unverified_request
+	if ForgeryProtection::QueryTracker.sql_events.any? { |e| e.write? } && !verified_request?
+          raise AttemptError, "A database update occurred for an unverified request"
 	end
       end
     end
 
     def handle_unverified_request
-      raise AttemptError
+      # Rails allows get requests, so to preserve that behavior
+      # doing nothing if unverified request was a GET request.
+      request.get? || super
     end
   end
 end
--- test/controller_test.rb
+++ test/controller_test.rb
@@ -4,22 +4,22 @@ require 'action_dispatch'
 
 class ControllerTest < ActionController::TestCase
   class Controller < ActionController::Base
-    skip_forgery_protection :only => :touch
+    protect_from_forgery :except => [ :unprotected_read, :unprotected_write ]
+    permit_unverified_state_changes :only => [ :db_permitted_read, :db_permitted_write ]
 
     def read
       render :json => post
     end
 
-    def update
-      post.update_attributes! :message => params[:message]
+    def write
+      post.update_attribute :message, params[:message]
 
       render :json => post
     end
 
-    def touch
-      post.touch
-
-      render :json => post
+    %w(unprotected db_permitted).each do |prefix|
+      define_method("#{prefix}_read") { read }
+      define_method("#{prefix}_write") { write }
     end
 
     private
@@ -48,32 +48,46 @@ class ControllerTest < ActionController::TestCase
   end
 
   def test_reads
-    get :read, :id => Post.last
+    %w(read unprotected_read db_permitted_read).each do |read_action|
+      get read_action, :id => Post.last
+    end
   end
 
-  def test_verified_get
-    assert_nothing_raised { get :update, :id => Post.last, :authenticity_token => @csrf_token, :message => 'bye' }
+  def test_verified_get_write
+    assert_nothing_raised { get :write, :id => Post.last, :authenticity_token => @csrf_token, :message => 'bye' }
 
     assert_equal 'bye', Post.last.message
   end
 
-  def test_verified_post
-    assert_nothing_raised { post :update, :id => Post.last, :authenticity_token => @csrf_token, :message => 'bye' }
+  def test_verified_post_write
+    assert_nothing_raised { post :write, :id => Post.last, :authenticity_token => @csrf_token, :message => 'bye' }
 
     assert_equal 'bye', Post.last.message
   end
 
-  def test_unverified_get
-    assert_raises(ForgeryProtection::AttemptError) { get :update, :id => Post.last, :authenticity_token => 'bad token', :message => 'bye' }
+  def test_unverified_get_write
+    assert_raises(ForgeryProtection::AttemptError) { get :write, :id => Post.last, :authenticity_token => 'bad token', :message => 'bye' }
   end
 
-  def test_unverified_post
-    assert_raises(ForgeryProtection::AttemptError) { post :update, :id => Post.last, :authenticity_token => 'bad token', :message => 'bye' }
+  def test_unverified_post_write
+    assert_raises(ForgeryProtection::AttemptError) { post :write, :id => Post.last, :authenticity_token => 'bad token', :message => 'bye' }
   end
 
-  def test_skipped_verification
+  def test_unprotected_get_write
+    assert_raises(ForgeryProtection::AttemptError) do
+      get :unprotected_write, :id => Post.last, :authenticity_token => 'bad token', :message => 'bye' 
+    end
+  end
+
+  def test_unprotected_post_write
+    assert_raises(ForgeryProtection::AttemptError) do
+      post :unprotected_write, :id => Post.last, :authenticity_token => 'bad token', :message => 'bye' 
+    end
+  end
+
+  def test_db_permitted_verification_write
     before = Post.last.updated_at
-    assert_nothing_raised { get :touch, :id => Post.last }
+    assert_nothing_raised { get :db_permitted_write, :id => Post.last }
 
     assert_not_equal before, Post.last.updated_at, "Should update the record"
   end
